b4d9d91e36546d0dca51737befcb3257dd3498a3
Shuji OCHI <ponpoko1968@gmail.com>
2016-05-21 09:59:27 +0900

    Initial Commit

---
+++ PaperPrototypeHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaperPrototypeHelper
+//
+//  Created by 越智修司 on 2016/05/21.
+//  Copyright © 2016年 shuji ochi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
